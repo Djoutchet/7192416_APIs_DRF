602b342d276dfa6394a89cbe43898ece6fce6780
kemlekeu <kemlekeugabin@yahoo.fr>
2023-09-24 09:21:34 +0200

    imbrication et filtrage des serializers et ajout d'une pagination

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,14 +1,22 @@
 from rest_framework import serializers
 from shop.models import Category, Product, Article
 
-class SerializerCategory(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = [ 'id', 'name', 'description', 'active', 'date_created', 'date_updated']
 
 
 class SerializersProduct(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = [ 'id', 'name','date_created', 'date_updated', 'description', 'category']
+
     
+
+class SerializerCategory(serializers.ModelSerializer):
+    products = serializers.SerializerMethodField()
+    class Meta:
+        model = Category
+        fields = [ 'id', 'name', 'description', 'active', 'date_created', 'date_updated','products']
+        
+    def get_products(self, instance):
+        queryset = instance.products.filter(active=True)
+        serializers = SerializersProduct(queryset, many= True)
+        return serializers.data
